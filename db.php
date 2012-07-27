383a581be867af6a7e3893d53e2879bb77ec809a
s3310384 <s3310384@student.rmit.edu.au>
2012-07-27 15:11:58 +1000

    Add db.php

---
+++ db.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Define the DB connection details separately so that 
+ * they are not publicly available in github. 
+ *
+ */
+
+/**
+ * Hostname and port mysql is running on (can't use localhost)
+ */
+define('DB_HOST',   'yallara.cs.rmit.edu.au:58152');
+/**
+ * Name of database to connect to
+ */
+define('DB_NAME',   'winestore');
+/**
+ * Username to connect with
+ */
+define('DB_USER',   's3310384');
+
+/**
+ * Password to connect with
+ */
+define('DB_PW',     's3310384');
+?>
